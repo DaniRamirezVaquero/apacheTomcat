08af27ad1cb8058257410d225c464455c2b15a62
DaniRamirezVaquero <dramvaq616@g.educaand.es>
2024-01-31 13:48:03 +0100

    fixed deploy.sh

--- AppDeploy.sh
+++ AppDeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 6. Descargamos nuestro repositorio de GitHub
-apt-intall git -y
+apt intall git -y
 
 mkdir /home/tmp
 cd /home/tmp
@@ -13,4 +13,4 @@ cd /apacheTomcat/app
 ./gradlew war
 
 # 8. Copiamos el war a la carpeta de tomcat
-mv /app/build/libs/*.war /opt/tomcat/webapps/hola.war
+mv build/libs/*.war /opt/tomcat/webapps/hola.war
--- deleteStack.sh
+++ deleteStack.sh
@@ -8,6 +8,9 @@ REGION="us-east-1"
 echo "Deleting stack..."
 aws cloudformation delete-stack --stack-name $STACK_NAME --region $REGION
 
+echo "Waiting for stack to be deleted..."
+aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME --region $REGION
+
 if [ $? -eq 0 ]; then
     echo "Deleting stack successfully"
 fi
